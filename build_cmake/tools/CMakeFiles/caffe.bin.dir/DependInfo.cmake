98ffdfb3416f1fb0a83ebca9897755c9dec30f9b
BannyStone <chopine.jb@gmail.com>
2018-01-15 17:09:22 +0800

    Add Ndp

---
+++ build_cmake/tools/CMakeFiles/caffe.bin.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/media/SSD/zhoulei/workspace/Caffes/caffe_nd/tools/caffe.cpp" "/media/SSD/zhoulei/workspace/Caffes/caffe_nd/build_cmake/tools/CMakeFiles/caffe.bin.dir/caffe.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GTEST_USE_OWN_TR1_TUPLE"
+  "USE_CUDNN"
+  "USE_MPI"
+  "WITH_PYTHON_LAYER"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/media/SSD/zhoulei/workspace/Caffes/caffe_nd/build_cmake/src/caffe/CMakeFiles/caffe.dir/DependInfo.cmake"
+  "/media/SSD/zhoulei/workspace/Caffes/caffe_nd/build_cmake/src/caffe/CMakeFiles/proto.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "include"
+  "/usr/local/cuda/include"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/include/atlas"
+  "/usr/include/python2.7"
+  "/usr/include/x86_64-linux-gnu/python2.7"
+  "/usr/local/lib/python2.7/dist-packages/numpy/core/include"
+  "/media/SSD/zhoulei/workspace/Tools/openmpi/include"
+  "../include"
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
